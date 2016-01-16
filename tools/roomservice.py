a270e9355bfee4862fbfca032a311151d6367f00
zodex <mimirower45@wp.pl>
2016-01-16 20:15:34 +0100

    Make it Crystal

--- tools/roomservice.py
+++ tools/roomservice.py
@@ -36,16 +36,16 @@ except ImportError:
 
 # Config
 # set this to the default remote to use in repo
-default_rem = "omnirom"
+default_rem = "ZZGroup"
 # set this to the default revision to use (branch/tag name)
 default_rev = "android-6.0"
 # set this to the remote that you use for projects from your team repos
-# example fetch="https://github.com/omnirom"
-default_team_rem = "omnirom"
+# example fetch="https://github.com/ZZGroup"
+default_team_rem = "ZZGroup"
 # this shouldn't change unless google makes changes
 local_manifest_dir = ".repo/local_manifests"
 # change this to your name on github (or equivalent hosting)
-android_team = "omnirom"
+android_team = "ZZGroup"
 # url to gerrit repository
 gerrit_url = "gerrit.omnirom.org"
 
@@ -204,7 +204,7 @@ def parse_device_from_folder(device):
 
 
 def parse_dependency_file(location):
-    dep_file = "omni.dependencies"
+    dep_file = "zz.dependencies"
     dep_location = '/'.join([location, dep_file])
     if not os.path.isfile(dep_location):
         print("WARNING: %s file not found" % dep_location)
